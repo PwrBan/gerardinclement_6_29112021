9e0d070ec5e35b470cdf1f5be0d94e78cd4e6d6e
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-10-13 09:31:59 +0200

    Fichier de base

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+mongoose.connect('mongodb+srv://PwrBan:Ol0Bztogbg19tXFq@cluster0.juf4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
